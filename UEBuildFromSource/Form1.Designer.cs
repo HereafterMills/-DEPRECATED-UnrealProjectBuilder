65465963988d1d445a9d546ec6a18d1d74df8eca
Raahil Kulshreshtha <raahil@hasdesk.com>
2019-02-22 21:40:13 +0530

    Midway

--- UEBuildFromSource/Form1.Designer.cs
+++ UEBuildFromSource/Form1.Designer.cs
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.projectPath = new System.Windows.Forms.TextBox();
+            this.projectPathTes = new System.Windows.Forms.Label();
+            this.EngineVersions = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(560, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 70);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Backup Project Folder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(173, 428);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Build Settings";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(391, 428);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Archive Settings";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // projectPath
+            // 
+            this.projectPath.Location = new System.Drawing.Point(135, 200);
+            this.projectPath.Name = "projectPath";
+            this.projectPath.Size = new System.Drawing.Size(227, 20);
+            this.projectPath.TabIndex = 4;
+            this.projectPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // projectPathTes
+            // 
+            this.projectPathTes.AutoSize = true;
+            this.projectPathTes.Location = new System.Drawing.Point(56, 203);
+            this.projectPathTes.Name = "projectPathTes";
+            this.projectPathTes.Size = new System.Drawing.Size(73, 13);
+            this.projectPathTes.TabIndex = 5;
+            this.projectPathTes.Text = ".uproject Path";
+            // 
+            // EngineVersions
+            // 
+            this.EngineVersions.FormattingEnabled = true;
+            this.EngineVersions.Location = new System.Drawing.Point(135, 226);
+            this.EngineVersions.Name = "EngineVersions";
+            this.EngineVersions.Size = new System.Drawing.Size(293, 21);
+            this.EngineVersions.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(146, 148);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(227, 20);
+            this.test.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(761, 566);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EngineVersions);
+            this.Controls.Add(this.projectPathTes);
+            this.Controls.Add(this.projectPath);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox projectPath;
+        private System.Windows.Forms.Label projectPathTes;
+        private System.Windows.Forms.ComboBox EngineVersions;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox test;
     }
 }
 
--- UEBuildFromSource/Form1.cs
+++ UEBuildFromSource/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
 
 namespace UEBuildFromSource
 {
@@ -15,6 +17,69 @@ namespace UEBuildFromSource
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\\Epic Games\\Unreal Engine\\Builds");
+            if (key != null)
+            {
+                foreach (var v in key.GetValueNames())
+                {
+                   //Console.WriteLine(key.GetValue(v));
+                   EngineVersions.Items.Add(key.GetValue(v));
+                }
+            }
+            //RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\\Epic Games\\Unreal Engine\\");
+            //if (key != null)
+            //{
+            //    foreach (var v in key.GetValueNames())
+            //    {
+            //        Console.WriteLine(key.GetValue(v));
+            //        EngineVersions.Items.Add(key.GetValue(v));
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Key was null");
+
+            //}
+
+
+
+
+            string text = File.ReadAllText("A:\\Wuerth\\Warehouse\\UE4\\WarehouseVRsssssssssss\\WuerthTrainingDemo.uproject", Encoding.UTF8);
+            string[] allLines = text.Split('{');
+            var version = allLines[2];
+            allLines = version.Split('}');
+            version = allLines[0];
+
+            test.Text = version;
+            //Console.Write(allLines[1]);
+            //Console.Write(allLines[2]);
+            //Console.Write(allLines[3]);
+            //Console.Write(allLines[4]);
+            Console.Write(version);
+            //Console.Write(allLines[6]);
+
+
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //string text = File.ReadAllText(projectPath.Text, Encoding.UTF8);
+            //string[] allLines = text.Split('\"');
+            //var version = allLines[3];
+
+
+            //Console.Write(version);
+
+
+
         }
     }
 }
